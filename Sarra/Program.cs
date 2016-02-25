bb9a1b4b5023d163782dab3d38a797f3036be161
Luke Liukonen <mfanzone@hotmail.com>
2016-02-24 18:41:36 -0600

    Initial check-in of module Sarra

---
+++ Sarra/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sarra
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			System.Speech.Synthesis.SpeechSynthesizer voice = new System.Speech.Synthesis.SpeechSynthesizer ();
+			voice.s
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
